767b122cd4e533c51a51e23e2dbfe7349445a10c
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-03-30 15:23:14 -0400

    UI changes: added a label, better user facing text

--- DonationTracker_DesktopLayer/gtk-gui/MainWindow.cs
+++ DonationTracker_DesktopLayer/gtk-gui/MainWindow.cs
@@ -3,49 +3,20 @@
 
 public partial class MainWindow
 {
-	private global::Gtk.UIManager UIManager;
-
-	private global::Gtk.Action dfdAction;
-
-	private global::Gtk.Action dfdgdfsAction;
-
-	private global::Gtk.Action dsfsdfAction;
-
-	private global::Gtk.Action dfdAction1;
-
-	private global::Gtk.Action abdcAction;
-
 	private global::Gtk.Fixed fixed1;
-
-	private global::Gtk.Button button2;
 
 	private global::Gtk.Button ShowTableButton;
 
 	private global::Gtk.Table DonorsTableView;
 
+	private global::Gtk.Label label1;
+
+	private global::Gtk.Button button2;
+
 	protected virtual void Build()
 	{
 		global::Stetic.Gui.Initialize(this);
 		// Widget MainWindow
-		this.UIManager = new global::Gtk.UIManager();
-		global::Gtk.ActionGroup w1 = new global::Gtk.ActionGroup("Default");
-		this.dfdAction = new global::Gtk.Action("dfdAction", global::Mono.Unix.Catalog.GetString("dfd"), null, null);
-		this.dfdAction.ShortLabel = global::Mono.Unix.Catalog.GetString("dfd");
-		w1.Add(this.dfdAction, null);
-		this.dfdgdfsAction = new global::Gtk.Action("dfdgdfsAction", global::Mono.Unix.Catalog.GetString("dfdgdfs"), null, null);
-		this.dfdgdfsAction.ShortLabel = global::Mono.Unix.Catalog.GetString("dfdgdfs");
-		w1.Add(this.dfdgdfsAction, null);
-		this.dsfsdfAction = new global::Gtk.Action("dsfsdfAction", global::Mono.Unix.Catalog.GetString("dsfsdf"), null, null);
-		this.dsfsdfAction.ShortLabel = global::Mono.Unix.Catalog.GetString("dsfsdf");
-		w1.Add(this.dsfsdfAction, null);
-		this.dfdAction1 = new global::Gtk.Action("dfdAction1", global::Mono.Unix.Catalog.GetString("dfd"), null, null);
-		this.dfdAction1.ShortLabel = global::Mono.Unix.Catalog.GetString("dfd");
-		w1.Add(this.dfdAction1, null);
-		this.abdcAction = new global::Gtk.Action("abdcAction", global::Mono.Unix.Catalog.GetString("abdc"), null, null);
-		this.abdcAction.ShortLabel = global::Mono.Unix.Catalog.GetString("abdc");
-		w1.Add(this.abdcAction, null);
-		this.UIManager.InsertActionGroup(w1, 0);
-		this.AddAccelGroup(this.UIManager.AccelGroup);
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString("Donor Tracker");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
@@ -54,25 +25,15 @@ public partial class MainWindow
 		this.fixed1.Name = "fixed1";
 		this.fixed1.HasWindow = false;
 		// Container child fixed1.Gtk.Fixed+FixedChild
-		this.button2 = new global::Gtk.Button();
-		this.button2.CanFocus = true;
-		this.button2.Name = "button2";
-		this.button2.UseUnderline = true;
-		this.button2.Label = global::Mono.Unix.Catalog.GetString("Start Adding Donors");
-		this.fixed1.Add(this.button2);
-		global::Gtk.Fixed.FixedChild w2 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.button2]));
-		w2.X = 206;
-		w2.Y = 34;
-		// Container child fixed1.Gtk.Fixed+FixedChild
 		this.ShowTableButton = new global::Gtk.Button();
 		this.ShowTableButton.CanFocus = true;
 		this.ShowTableButton.Name = "ShowTableButton";
 		this.ShowTableButton.UseUnderline = true;
-		this.ShowTableButton.Label = global::Mono.Unix.Catalog.GetString("Show Donors");
+		this.ShowTableButton.Label = global::Mono.Unix.Catalog.GetString("Show Donations");
 		this.fixed1.Add(this.ShowTableButton);
-		global::Gtk.Fixed.FixedChild w3 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.ShowTableButton]));
-		w3.X = 389;
-		w3.Y = 179;
+		global::Gtk.Fixed.FixedChild w1 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.ShowTableButton]));
+		w1.X = 389;
+		w1.Y = 179;
 		// Container child fixed1.Gtk.Fixed+FixedChild
 		this.DonorsTableView = new global::Gtk.Table(((uint)(4)), ((uint)(3)), false);
 		this.DonorsTableView.Name = "DonorsTableView";
@@ -80,19 +41,39 @@ public partial class MainWindow
 		this.DonorsTableView.ColumnSpacing = ((uint)(6));
 		this.DonorsTableView.BorderWidth = ((uint)(3));
 		this.fixed1.Add(this.DonorsTableView);
-		global::Gtk.Fixed.FixedChild w4 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.DonorsTableView]));
-		w4.X = 53;
-		w4.Y = 148;
+		global::Gtk.Fixed.FixedChild w2 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.DonorsTableView]));
+		w2.X = 53;
+		w2.Y = 148;
+		// Container child fixed1.Gtk.Fixed+FixedChild
+		this.label1 = new global::Gtk.Label();
+		this.label1.Name = "label1";
+		this.label1.LabelProp = global::Mono.Unix.Catalog.GetString("The purpose of this program is to keep track of monetary donations, and who gave " +
+				"them. The target use case is for non-profit organizations.");
+		this.label1.Wrap = true;
+		this.fixed1.Add(this.label1);
+		global::Gtk.Fixed.FixedChild w3 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.label1]));
+		w3.X = 130;
+		w3.Y = 30;
+		// Container child fixed1.Gtk.Fixed+FixedChild
+		this.button2 = new global::Gtk.Button();
+		this.button2.CanFocus = true;
+		this.button2.Name = "button2";
+		this.button2.UseUnderline = true;
+		this.button2.Label = global::Mono.Unix.Catalog.GetString("Add Donations");
+		this.fixed1.Add(this.button2);
+		global::Gtk.Fixed.FixedChild w4 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.button2]));
+		w4.X = 397;
+		w4.Y = 141;
 		this.Add(this.fixed1);
 		if ((this.Child != null))
 		{
 			this.Child.ShowAll();
 		}
-		this.DefaultWidth = 525;
+		this.DefaultWidth = 874;
 		this.DefaultHeight = 374;
 		this.Show();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler(this.OnDeleteEvent);
-		this.button2.Clicked += new global::System.EventHandler(this.ShowDonorAdditionWindow);
 		this.ShowTableButton.Clicked += new global::System.EventHandler(this.OnShowTableButtonClicked);
+		this.button2.Clicked += new global::System.EventHandler(this.ShowDonorAdditionWindow);
 	}
 }
