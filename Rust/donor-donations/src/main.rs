50fa460024f37072035d5ca5b67a08a5cc13eb37
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-04-01 18:17:34 -0400

    Minimal working Rust GTK (3) example

--- Rust/donor-donations/src/main.rs
+++ Rust/donor-donations/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+//  The following code was taken directly from
+// https://www.gtk.org/docs/language-bindings/rust/
+// On April 1, 2020.
+
+use gio::prelude::*;
+use glib::clone;
+use gtk::prelude::*;
+
+// When the application is launched…
+fn on_activate(application: &gtk::Application) {
+    // … create a new window …
+    let window = gtk::ApplicationWindow::new(application);
+    // … with a button in it …
+    let button = gtk::Button::new_with_label("Hello World!");
+    // … which closes the window when clicked
+    button.connect_clicked(clone!(@weak window => move |_| window.destroy()));
+    window.add(&button);
+    window.show_all();
 }
+
+fn main() {
+    // Create a new application
+    let app = gtk::Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
+        .expect("Initialization failed...");
+    app.connect_activate(|app| on_activate(app));
+    // Run the application
+    app.run(&std::env::args().collect::<Vec<_>>());
+}
+
