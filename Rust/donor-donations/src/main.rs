347cdfb0793116b9609f3ee38bfce5fa9693b9e4
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-04-02 01:32:33 -0400

    Add labels to GUI

--- Rust/donor-donations/src/main.rs
+++ Rust/donor-donations/src/main.rs
@@ -17,21 +17,34 @@ fn on_activate(application: &gtk::Application)
 
 
     let vbox = gtk::Box::new(gtk::Orientation::Vertical, 12);
+    let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 12);
+    let button_box = gtk::Box::new(gtk::Orientation::Vertical, 10);
 
     let show_donations_button = gtk::Button::new_with_label("Show Donations");
     let show_total_amount_button = gtk::Button::new_with_label("Calculate Total Donation Amount");
+    let intro_label = gtk::Label::new(Some("The purpose of this program is to keep track of monetary donations, and who gave them. The target use case is for non-profit organizations."));
+    intro_label.set_line_wrap(true);
+    let table_title_label = gtk::Label::new(Some("Select button on right to show data"));
     let label = gtk::Label::new(None);
     let total_donation_prelabel = gtk::Label::new(Some("Total donation amount:"));
     let total_donation_label = gtk::Label::new(None);
 
     window.add(&vbox);
 
+    vbox.add(&intro_label);
 
-    vbox.add(&show_donations_button);
-    vbox.add(&show_total_amount_button);
-    vbox.add(&label);
-    vbox.add(&total_donation_prelabel);
-    vbox.add(&total_donation_label);
+    button_box.add(&show_donations_button);
+    button_box.add(&show_total_amount_button);
+    button_box.add(&total_donation_prelabel);
+    button_box.add(&total_donation_label);
+
+    let vbox_of_table = gtk::Box::new(gtk::Orientation::Vertical, 12);
+    vbox_of_table.add(&table_title_label);
+    vbox_of_table.add(&label);
+
+    hbox.add(&vbox_of_table);
+    hbox.add(&button_box);
+    vbox.add(&hbox);
 
     show_donations_button.connect_clicked(clone!(@weak window => move |button| label.set_label(&database_bridge::get_donations().to_string())));
     show_total_amount_button.connect_clicked(clone!(@weak window => move |button| total_donation_label.set_label(&database_bridge::calculate_total_donation_amount().to_string())));
