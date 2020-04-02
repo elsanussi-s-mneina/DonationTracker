860d199a8e30ea3246c9fd29eddf455de3a27b15
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-04-01 21:49:47 -0400

    Also get the donation amount from the database,
    Note: we had to use rust_decimal, and its postgres feature for this to work.

--- Rust/donor-donations/src/database_bridge.rs
+++ Rust/donor-donations/src/database_bridge.rs
@@ -1,15 +1,18 @@
 use postgres::{Client, NoTls};
+use rust_decimal::Decimal;
 
 pub fn access_database()
 {
 
     let mut client = Client::connect("host=localhost user=donation_tracker_user dbname=donation_tracking password=secret1secret", NoTls).unwrap();
 
-    for row in client.query("SELECT donor.id, donor.first_name, donor.last_name FROM donation INNER JOIN donor ON donation.donor_id = donor.id;", &[]).unwrap()
+    for row in client.query("SELECT donor.id, donor.first_name, donor.last_name, donation.donation_amount FROM donation INNER JOIN donor ON donation.donor_id = donor.id;", &[]).unwrap()
     {
         let id: i32 = row.get(0);
         let first_name: &str = row.get(1);
         let last_name: &str = row.get(2);
-        println!("found person: {}\t{}\t{}", id, first_name, last_name);
+        let donation_amount : Decimal  = row.get(3);
+
+        println!("found person: {}\t{}\t{}\t{}", id, first_name, last_name, donation_amount);
     }
 }
