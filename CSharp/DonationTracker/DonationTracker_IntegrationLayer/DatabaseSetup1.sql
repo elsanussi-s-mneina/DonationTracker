3bd64e7b9593ce29cca846546495ce75ac89644c
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-07-24 11:32:24 -0400

    Fix problem preventing database creation
    Postgresql  12.3 complains that the collation does not match the encoding. I chose to use template 0 to fix this problem.

--- CSharp/DonationTracker/DonationTracker_IntegrationLayer/DatabaseSetup1.sql
+++ CSharp/DonationTracker/DonationTracker_IntegrationLayer/DatabaseSetup1.sql
@@ -12,6 +12,6 @@
 CREATE USER donation_tracker_user WITH PASSWORD 'secret1secret';
 
 
-CREATE DATABASE donation_tracking WITH OWNER donation_tracker_user ENCODING='UTF-8' LC_COLLATE='en_US.UTF-8' LC_CTYPE='en_US.UTF-8';
+CREATE DATABASE donation_tracking WITH OWNER donation_tracker_user ENCODING='UTF-8' LC_COLLATE='en_US.UTF-8' LC_CTYPE='en_US.UTF-8' TEMPLATE template0;
 
 GRANT ALL PRIVILEGES ON DATABASE donation_tracking TO donation_tracker_user;
